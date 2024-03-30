554b6edf9a7eb842013c1f7cda6b0561fa298352
Piotr Sowa <piotrsowa87@gmail.com>
2024-03-30 13:57:46 +0100

    [task] Add handling for users

--- client/app.js
+++ client/app.js
@@ -1,3 +1,17 @@
+const socket = io({
+    autoConnect: true  // false - do not open channel on init
+  });    // default socets listen client server localhost:8000
+// socket.open()        // open active channel
+//socket.connect("http://example.com");
+//socket.on('message', (event) => addMessage(event.author, event.content));
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('addUser', ({name}) => {
+    addSystemMessage(`${name} has joined the conversation!`);    
+});
+socket.on('removeUser', ({name}) => {
+    addSystemMessage(`${name} has left the conversation... :(`);    
+});
+
 const elements = {
     loginForm: document.querySelector('#welcome-form'),
     messageSection: document.querySelector('#messages-section'),
@@ -9,7 +23,6 @@ const elements = {
 
 let userName = '';
 
-
 const login = (e) => {
     e.preventDefault();
     const user = elements.userNameInput.value;
@@ -20,7 +33,17 @@ const login = (e) => {
         elements.userNameInput.value= '';
         elements.loginForm.classList.remove('show');
         elements.messageSection.classList.add('show');
+        socket.emit('login', { name: userName})
     }
+}
+
+const addSystemMessage = (content) => {
+    const message = document.createElement('li');
+    message.classList.add('message', 'message--received');
+    message.innerHTML =
+        `<h3 class="message__author">Chat Bot</h3>
+        <div class="message__content message__system">${content}</div>`
+    elements.messagesList.insertAdjacentElement('beforeend', message);
 }
 
 const addMessage = (author, content) => {
@@ -35,12 +58,14 @@ const addMessage = (author, content) => {
 
 const sendMessage = (e) => {
     e.preventDefault();
-    const msg = elements.messageContentInput.value;
-    if(!msg) {
+    const messageContent = elements.messageContentInput.value;
+    if(!messageContent) {
         alert('Missing message');
     } else {
         elements.messageContentInput.value= '';
-        addMessage(userName, msg);
+        addMessage(userName, messageContent);
+        socket.emit('message', { author: userName, content: messageContent })
+
     }
     console.log('send msg')
 };
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const socket = require('socket.io');
+
+const path = require('path');
 
 const messages = [];
+let users = [];
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client')));
@@ -12,6 +15,40 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
     console.log('Server running on port 8000')
-})
+});
+
+const io = socket(server);  // integrate websocket io module with running server
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('login', (user) => {
+        console.log('new user logged ', user, socket.id);
+        users.push({...user, id: socket.id});
+        socket.broadcast.emit('addUser', { name: user.name });
+    })
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+//        socket.emit('message', message)   // emit event to socket client
+//        io.to('IIr5o6g-jQDfK3vcAAAC').emit('message', 'For your eyes only!'); // emit event to client with id
+        socket.broadcast.emit('message', message);  // emit event to all connected clients expect this 
+      });
+    socket.on('disconnect', () => {
+       let removedUser = {};
+        users = users.filter(user => {
+            if(user.id === socket.id) {
+                removedUser = user;
+                return false;
+            }
+            return true});
+        socket.broadcast.emit('removeUser', { name: removedUser.name });
+        console.log('Remove user: ', removedUser);
+        console.log('Oh, socket ' + socket.id + ' has left', users);
+    });
+    console.log('I\'ve added a listener on message event \n');
+});
+
+// io.emit('message', message); // emits event to all connected clients
+
